1a9abb69622c3175813895cd4f35d3b1228e90d1
balboa9623 <mathewrichu16@gmail.com>
2020-08-02 18:55:58 -0500

    Implemented adding a new transaction, calling another view right after adding a new transaction (work needed to be done here).

--- stock_investment/transfers/urls.py
+++ stock_investment/transfers/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'transfers'
 
 urlpatterns = [
-    path('by/(?<username>[-\w]+)/', views.UserTransferList.as_view(), name='user_all'),
-    # path('new/', views.TransferCreateView.as_view(), name='transfer_new'),
-    # # 'username' & 'pk' is returned from get_absolute_url() function defined in models.py.
-    # # the path finds details of a single transaction, based on the username and pk provided.
-    # path('usr/(?P<username>[-\W]+)/(?P<pk>\d+)/', views.TransferDetailsView.as_view(), name='single'),
+    path('usr/(?P<username>[-\w]+)/', views.UserTransferList.as_view(), name='user_all'),
+    path('new/', views.CreateTransferView.as_view(), name='new_transfer'),
+    # 'username' & 'pk' is returned from get_absolute_url() function defined in models.py.
+    # the path finds details of a single transaction, based on the username and pk provided.
+    path('usr/(?P<username>[-\W]+)/(?P<pk>\d+)/', views.TransferDetailsView.as_view(), name='single'),
 ]
--- stock_investment/transfers/views.py
+++ stock_investment/transfers/views.py
@@ -4,41 +4,44 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 # from .forms import TransferCreateForm
 from .models import AppTransfer
+from django import forms
+from braces.views import SelectRelatedMixin
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 # Create your views here.
 class UserTransferList(LoginRequiredMixin, generic.ListView):
-    # TODO
-    #raise NotImplementedError
     model = AppTransfer
     template_name = 'transfers/user_transfer_list.html'
-
     # For future reference: get_queryset() is used to run a specific query on the database.
     # Here, the query retrieves the transactions make by the currently active user.
+    # def get_queryset(self):
+    #     try:
+
+
+
+class CreateTransferView(LoginRequiredMixin, generic.CreateView, SelectRelatedMixin):
+    # don't need to handle login_url here. It is taken care of
+    # in the settings.py file
+    fields = ('transfer_amount', 'date_initiated', 'date_fulfilled')
+
+    model = AppTransfer
+    template_name = "transfers/transfer_create.html"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+
+
+class TransferDetailsView(LoginRequiredMixin, generic.DetailView):
+    model = AppTransfer
+    select_related = ('transfer_amount', 'date_initiated', 'date_fulfilled')
+    template_name = 'transfers/transfer_details.html'
+
     def get_queryset(self):
-        try:
-            # figure out how to get the currently active user
-            app_user = AppTransfer.objects.filter(user=self.request.user)
-        except User.DoesNotExist:
-            raise Http404
-        else:
-            # return self.app_transfer_user.app_user.all()
-            return self.app_user.app_user.all()
+        queryset = super().get_queryset()
+        return queryset.filter(user__username__iexact=self.kwargs.get('username'))
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['app_user'] = self.app_user
-        return context
-
-
-# class TransferCreateView(LoginRequiredMixin, generic.CreateView):
-#     # TODO
-#     # don't need to handle login_url here. It is taken care of
-#     # in the settings.py file
-#     raise NotImplementedError
-#
-#
-# class TransferDetailsView(LoginRequiredMixin, generic.DetailView):
-#     # TODO
-#     raise NotImplementedError
