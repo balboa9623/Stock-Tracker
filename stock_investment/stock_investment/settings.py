a5d344d68685c7c6b644cc1648fccd172c5acce9
balboa9623 <56138239+balboa9623@users.noreply.github.com>
2020-11-27 10:57:43 -0600

    Update settings.py

--- stock_investment/stock_investment/settings.py
+++ stock_investment/stock_investment/settings.py
@@ -22,7 +22,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jk)_6gkz92kas7)6xufv#g2)!=wx5(z)_-xd16o_rkxs8@z0zv'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
